9266dffe42453047f44900094a2576425db4f493
Robert McDonnell <rcmcd@hotmail.com>
2021-05-08 23:02:33 -0600

    Added libc

---
+++ libc/strings.c
@@ -0,0 +1,17 @@
+#include "strings.h"
+
+int strlen(char s[]) {
+    int i = 0;
+    while (s[i] != '\0')
+        i++;
+    return i;
+}
+
+int strcmp(char s1[], char s2[]) {
+    int i;
+    for (i = 0; s1[i] == s2[i]; i++) {
+        if (s1[i] == '\0')
+            return 0;
+    }
+    return s1[i] - s2[i];
+}
